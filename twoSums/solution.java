f26c64e889350d614073c1cc16dc88df848a332c
Evan Vu <vuc@lafayette.edu>
2021-06-30 16:42:30 -0400

    Create solution.java

---
+++ twoSums/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int size = nums.length;
+        int[] res = new int[2];
+        for(int i = 0; i<size; i++){
+            if(i==size-1) continue;
+            int temp = target - nums[i];
+            for(int j = 1; j<size-i ;j++){
+                if(nums[i+j]==temp){
+                    res[0] = i;
+                    res[1] = i+j;
+                    return res;
+                }
+            }
+        }
+        return res;
+    }
+}
